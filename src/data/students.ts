beecbd6e5d36702768fac3b70975ae39542ab5fa
rodricross4-lab <rodricross4@gmail.com>
2026-05-14 07:03:16 -0300

    Add students seed

---
+++ src/data/students.ts
@@ -0,0 +1,18 @@
+export const studentsSeed = [
+  {
+    id:1,
+    name:'Rodrigo Santos',
+    goal:'Hipertrofia',
+    phase:'Bulking',
+    frequency:5,
+    priority:['Peitoral','Costas']
+  },
+  {
+    id:2,
+    name:'Valentina Rocha',
+    goal:'Glúteos',
+    phase:'Manutenção',
+    frequency:5,
+    priority:['Glúteos','Quadríceps']
+  }
+];
