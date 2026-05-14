7cd712a82d0c6a65989fe1a4261375b066acf233
rodricross4-lab <rodricross4@gmail.com>
2026-05-14 07:03:08 -0300

    Add exercise database

---
+++ src/data/exercises.ts
@@ -0,0 +1,9 @@
+export const exercises = [
+  { name:'Supino reto barra', group:'Peitoral', range:'5-10', rest:'2-4 min' },
+  { name:'Supino inclinado halteres', group:'Peitoral', range:'5-10', rest:'2-4 min' },
+  { name:'Remada curvada', group:'Costas', range:'5-10', rest:'2-4 min' },
+  { name:'Hack machine', group:'Quadríceps', range:'5-10', rest:'2-4 min' },
+  { name:'Mesa flexora', group:'Posteriores', range:'8-15', rest:'60-120s' },
+  { name:'Hip thrust', group:'Glúteos', range:'6-10', rest:'2-4 min' },
+  { name:'Elevação lateral cabo', group:'Deltoide lateral', range:'10-20', rest:'60-120s' }
+];
